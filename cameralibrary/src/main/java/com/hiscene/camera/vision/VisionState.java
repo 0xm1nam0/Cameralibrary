e03b8d03eefa03e2b67abf650aeb1f0aaf0d35e5
Minamo <kleinminamo@gmai.com>
2022-06-26 22:35:58 +0800

    Update : VisionState

--- cameralibrary/src/main/java/com/hiscene/camera/vision/VisionState.java
+++ cameralibrary/src/main/java/com/hiscene/camera/vision/VisionState.java
@@ -1,5 +1,11 @@
 package com.hiscene.camera.vision;
 
+/**
+ * @author  Minamo
+ * @e-mail  kleinminamo@gmail.com
+ * @time    2021/4/13
+ * @des     VisionState
+ */
 public enum VisionState {
     RECOGNIZED,
     TRACKING,
